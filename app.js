3b95f0e15af6d329079e02aba80e781a4ffdf618
lch <lch@163.com>
2017-03-26 23:31:00 +0800

    添加前台静态页面+配置路由

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
  * 应用程序入口文件
  */
 
+/**
+ * 01.加载模块
+ */
 //加载express模块
 var express = require('express');
 //加载模版处理模块
@@ -12,10 +15,14 @@ var mongoose = require('mongoose');
 //创建app应用 => NodeJS的Http.createServer();
 var app = express();
 
-//设置静态文件托管(用户访问的url以/public开始)
+/**
+ * 02.设置静态文件托管(用户访问的url以/public开始)
+ */
 app.use('/public', express.static(__dirname + '/public'));
 
-//配置应用模版
+/**
+ * 03.配置应用模版
+ */
 //定义当前应用使用的模版引擎
 //第一个参数：模版参数名称，也是模版后缀
 //第二个参数：解析处理模版的方法
@@ -28,36 +35,22 @@ app.set('view engine', 'html');
 swig.setDefaults({cache:false});
 
 /**
- * 首页
- *  req > request对象
- *  res > response对象
- *  next > 下一个处理函数
+ * 04.划分模块
  */
-app.get('/', function(req, res, next){
-    //res.send('<h1>欢迎登录NBlog!</h1>');
-
-    /**
-     * 读取views目录下的指定文件,解析并返回给客户端
-     * 第一个参数：模版文件(默认：index)
-     * 第二个参数：传递给模版的参数
-     */
-    res.render('index');
-});
-
-/**
- * 根据请求，划分成不同模块（前台，后台，ajax请求《api》）
- */
+//根据请求，划分成不同模块（前台，后台，ajax请求《api》）
 app.use('/admin', require('./routers/admin'));
 app.use('/api', require('./routers/api'));
 app.use('/', require('./routers/main'));
 
-//连接数据库
+/**
+ * 05.连接数据库
+ */
 mongoose.connect('mongodb://localhost:27018/NBlog', function(err){
     if(err){
         console.log('数据库连接失败');
     } else {
         console.log('数据库连接成功');
-        //监听请求
+        //06.监听端口
         app.listen(8383);
     }
 });
@@ -67,3 +60,19 @@ mongoose.connect('mongodb://localhost:27018/NBlog', function(err){
  * /public -> 静态 ->直接读取指定目录下的文件，返回给用户
  *         -> 动态 -> 处理业务逻辑，加载模版，解析模版 -> 返回数据
  */
+/**
+ * 首页
+ *  req > request对象
+ *  res > response对象
+ *  next > 下一个处理函数
+ */
+//app.get('/', function(req, res, next){
+//    //res.send('<h1>欢迎登录NBlog!</h1>');
+//
+//    /**
+//     * 读取views目录下的指定文件,解析并返回给客户端
+//     * 第一个参数：模版文件(默认：index)
+//     * 第二个参数：传递给模版的参数
+//     */
+//    res.render('index');
+//});
--- routers/main.js
+++ routers/main.js
@@ -1,12 +1,11 @@
 /**
  * Created by Lenovo on 2017/3/25.
  */
-
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.send('首页');
+    res.render('main/index');
 });
 
 module.exports = router;
